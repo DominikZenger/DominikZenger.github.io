2ff8c251d5f65b2c061426ba6631559a0d742d9d
BuildTools <Dominik-zenger@web.de>
2022-12-19 20:15:51 +0100

    bar charts added

--- scripts/schulweg_vergleich.js
+++ scripts/schulweg_vergleich.js
@@ -1,13 +1,45 @@
-var winter = false
+var winter_selected = false
 
 function change() {
-    if(winter) {
-        document.getElementById("bar-diagram").src = "../assets/images/sommer.png"
-        winter = false
+    let auto = document.getElementById("Auto")
+    let eAuto = document.getElementById("E-Auto")
+    let motorrad = document.getElementById("Motorrad")
+    let fahrrad = document.getElementById("Fahrrad")
+    let opnv = document.getElementById("OPNV")
+    let sonstiges = document.getElementById("Sonstiges")
+
+    let factor = "2"
+    if(winter_selected) {
+        // document.getElementById("bar-diagram").src = "../assets/images/sommer.png"
+        auto.style.height = "calc("+factor+" *4%)"
+        auto.getElementsByTagName("span").item(0).innerHTML = "4"
+        eAuto.style.height = "calc("+factor+" *0)"
+        eAuto.getElementsByTagName("span").item(0).innerHTML = "0"
+        motorrad.style.height = "calc("+factor+" *4%)"
+        motorrad.getElementsByTagName("span").item(0).innerHTML = "4"
+        fahrrad.style.height = "calc("+factor+" *39%)"
+        fahrrad.getElementsByTagName("span").item(0).innerHTML = "39"
+        opnv.style.height = "calc("+factor+" *10%)"
+        opnv.getElementsByTagName("span").item(0).innerHTML = "10"
+        sonstiges.style.height = "calc("+factor+" *5%)"
+        sonstiges.getElementsByTagName("span").item(0).innerHTML = "5"
+        winter_selected = false
     }
     else {
-        document.getElementById("bar-diagram").src = "../assets/images/winter.png"
-        winter = true
+        // document.getElementById("bar-diagram").src = "../assets/images/winter.png"
+        auto.style.height = "calc("+factor+" *13%)"
+        auto.getElementsByTagName("span").item(0).innerHTML = "13"
+        eAuto.style.height = "calc("+factor+" *1%)"
+        eAuto.getElementsByTagName("span").item(0).innerHTML = "1"
+        motorrad.style.height = "calc("+factor+" *1%)"
+        motorrad.getElementsByTagName("span").item(0).innerHTML = "1"
+        fahrrad.style.height = "calc("+factor+" *17%)"
+        fahrrad.getElementsByTagName("span").item(0).innerHTML = "17"
+        opnv.style.height = "calc("+factor+" *28%)"
+        opnv.getElementsByTagName("span").item(0).innerHTML = "28"
+        sonstiges.style.height = "calc("+factor+" *4%)"
+        sonstiges.getElementsByTagName("span").item(0).innerHTML = "4"
+        winter_selected = true
     }
 
 }
