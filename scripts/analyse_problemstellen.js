42a8129fb4b379de3e1c5cb7bd6ae17534537848
Gianluca Towae <towae.gianluca@gmail.com>
2023-01-19 09:11:51 +0100

    reset

---
+++ scripts/analyse_problemstellen.js
@@ -0,0 +1,244 @@
+var currentlyClicked = -1;
+
+let selectedGridItem = 0
+
+
+function selectArea(id) {
+    allCirclesRed();
+    allInfosInvisible();
+    
+    if(currentlyClicked === id) {
+        currentlyClicked = -1;
+        return;
+    }
+    currentlyClicked = id;
+
+    const name = document.getElementById("name");
+    const infos = document.getElementById("infos").children;
+    const circles = document.getElementsByClassName("circle");
+    const description = document.getElementById("description");
+
+    circles[id].style.backgroundColor = "blue";
+    setVisible(infos[id], true);
+    setVisible(name, true);
+    setVisible(document.getElementById("base-infos"), true);
+    let container;
+    switch(id) {
+        case 0: 
+            name.innerHTML = "Feldkirchner-Kreuzung";
+            description.innerHTML = Feldkirchner_Kreuzung_description;
+            container = document.getElementById("grid1");
+            if (!container.lastElementChild) {
+                createGrid(colorGridFeldkirchenerKreuzung, container);
+                select(selectedGridItem);
+            }
+            window.scrollTo(0, 1000);
+            break;
+        case 1:
+            name.innerHTML = "Kreuzung Ammerthalstraße und Feldkirchner Straße";
+            description.innerHTML = Ammerthalstrasse_description;
+            container = document.getElementById("grid2");
+            if (!container.lastElementChild) {
+                createGrid(colorGridAmmerthal, container);
+                select(selectedGridItem);
+            }
+            window.scrollTo(0, 1000);
+            break;
+        case 2: 
+            name.innerHTML = "Kirchheimer-Ei";
+            description.innerHTML = Kirchheimer_Ei_description;
+            container = document.getElementById("grid3");
+            if (!container.lastElementChild) {
+                createGrid(colorGridEi, container);
+                select(selectedGridItem);
+            }
+            window.scrollTo(0, 1000);
+            break;
+        case 3: 
+            name.innerHTML = "Kirchheimer-Kreuzung";
+            description.innerHTML = Kirchheimer_Kreuzung_description;
+            container = document.getElementById("grid4");
+            if (!container.lastElementChild) {
+                createGrid(colorGridKirchheimerKreuzung, container);
+                select(selectedGridItem);
+            }
+            window.scrollTo(0, 1000);
+            break;
+        case 4: 
+            name.innerHTML = "Florianstraße";
+            description.innerHTML = Florianstrasse_description
+            container = document.getElementById("grid5");
+            if (!container.lastElementChild) {
+                createGrid(colorGridflorianStrasse, container);
+                select(selectedGridItem);
+            }
+            window.scrollTo(0, 1000);
+            break;
+    }
+}
+
+function allInfosInvisible() {
+    setVisible(document.getElementById("name"), false);
+    setVisible(document.getElementById("base-infos"), false);
+    let infos = document.getElementById("infos").children;
+    for(var i = 0; i < infos.length; i++) {
+        setVisible(infos[i], false);
+    }
+}
+
+function setVisible(element, b) {
+    element.style.display = b ? "block" : "none";
+}
+
+function allCirclesRed() {
+    let circles = document.getElementsByClassName("circle");
+    for(var i = 0; i < circles.length; i++) {
+        circles[i].style.backgroundColor = "red";
+    }
+}
+
+const green = "#92D050"
+const yellow = "#F9DC05"
+const darkorange = "#FA8504"
+const brightorange = "#F9BB00"
+const red = "#FF0000"
+
+let colorGridFeldkirchenerKreuzung = [
+    ["#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000"],
+    ["#C1C1C1", green, yellow, brightorange, brightorange, brightorange, brightorange, brightorange, brightorange, brightorange, brightorange, brightorange, brightorange, yellow, green, green, green, yellow],
+    ["#FFFFFF", green, yellow, brightorange, brightorange, brightorange, brightorange, brightorange, brightorange, brightorange, brightorange, brightorange, brightorange, brightorange, green, brightorange, yellow, green],
+    ["#C1C1C1", green, yellow, brightorange, yellow, green, brightorange, brightorange, brightorange, brightorange, darkorange, brightorange, brightorange, brightorange, brightorange, yellow, green, green],
+    ["#FFFFFF", brightorange, brightorange, brightorange, brightorange, brightorange, brightorange, brightorange, brightorange, brightorange, brightorange, brightorange, darkorange, darkorange, brightorange, brightorange, brightorange, yellow],
+    ["#C1C1C1", green, brightorange, brightorange, brightorange, brightorange, brightorange, brightorange, brightorange, darkorange, darkorange, brightorange, darkorange, brightorange, brightorange, yellow, green, green],
+    ["#FFFFFF", green, yellow, brightorange, yellow, green, brightorange, brightorange, brightorange, brightorange, green, green, green, green, green, green, green, green],
+    ["#C1C1C1", green, green, yellow, yellow, green, brightorange, green, brightorange, brightorange, brightorange, green, green, green, green, green, green, green]
+]
+let colorGridAmmerthal = [
+    ["#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000"],
+    ["#C1C1C1", darkorange, brightorange, brightorange, yellow, yellow, brightorange, brightorange, brightorange, brightorange, brightorange, brightorange, yellow, yellow, yellow, brightorange, yellow, yellow, yellow],
+    ["#FFFFFF", darkorange, yellow, yellow, brightorange, brightorange, brightorange, brightorange, brightorange, brightorange, brightorange, yellow, green, yellow, yellow, yellow, yellow, brightorange],
+    ["#C1C1C1", darkorange, brightorange, yellow, brightorange, brightorange, brightorange, yellow, brightorange, brightorange, brightorange, brightorange, yellow, green, yellow, yellow, yellow, green],
+    ["#FFFFFF", brightorange, yellow, yellow, brightorange, brightorange, brightorange, brightorange, brightorange, darkorange, brightorange, brightorange, brightorange, yellow, yellow, yellow, green, yellow],
+    ["#C1C1C1", darkorange, brightorange, yellow, yellow, brightorange, brightorange, brightorange, yellow, brightorange, brightorange, yellow, yellow, yellow, yellow, green, green, green],
+    ["#FFFFFF", green, brightorange, yellow, yellow, green, brightorange, yellow, brightorange, brightorange, brightorange, yellow, yellow, green, green, yellow, yellow, green],
+    ["#C1C1C1", green, green, green, green, brightorange, brightorange, brightorange, brightorange, brightorange, brightorange, yellow, green, yellow, green, green, green, green]
+]
+let colorGridflorianStrasse = [
+    ["#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000"],
+    ["#C1C1C1", yellow, yellow, brightorange, brightorange, brightorange, brightorange, darkorange, brightorange, brightorange, brightorange, yellow, darkorange, brightorange, brightorange, darkorange, red, darkorange],
+    ["#FFFFFF", brightorange, brightorange, brightorange, brightorange, brightorange, brightorange, darkorange, darkorange, brightorange, brightorange, yellow, brightorange, brightorange, brightorange, brightorange, brightorange, brightorange],
+    ["#C1C1C1", brightorange, brightorange, darkorange, brightorange, brightorange, brightorange, brightorange, brightorange, brightorange, brightorange, brightorange, brightorange, brightorange, darkorange, brightorange, brightorange, darkorange],
+    ["#FFFFFF", yellow, yellow, brightorange, brightorange, brightorange, brightorange, darkorange, darkorange, brightorange, brightorange, brightorange, brightorange, brightorange, brightorange, brightorange, green, brightorange],
+    ["#C1C1C1", darkorange, brightorange, yellow, brightorange, darkorange, brightorange, brightorange, brightorange, brightorange, brightorange, brightorange, brightorange, brightorange, brightorange, darkorange, brightorange, brightorange],
+    ["#FFFFFF", brightorange, yellow, yellow, brightorange, darkorange, brightorange, brightorange, darkorange, darkorange, brightorange, brightorange, brightorange, brightorange, brightorange, brightorange, brightorange, darkorange],
+    ["#C1C1C1", yellow, yellow, darkorange, brightorange, brightorange, yellow, darkorange, darkorange, brightorange, brightorange, brightorange, brightorange, brightorange, brightorange, darkorange, darkorange, yellow]
+]
+let colorGridEi = [
+    ["#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000"],
+    ["#C1C1C1", green, brightorange, red, brightorange, brightorange, brightorange, green, green, brightorange, brightorange, brightorange, red, brightorange, brightorange, green, green, green],
+    ["#FFFFFF", green, brightorange, red, brightorange, green, green, brightorange, brightorange, brightorange, brightorange, brightorange, red, brightorange, brightorange, green, green, green],
+    ["#C1C1C1", green, brightorange, brightorange, brightorange, brightorange, green, brightorange, brightorange, brightorange, brightorange, brightorange, red, brightorange, green, green, green, green],
+    ["#FFFFFF", green, brightorange, red, brightorange, brightorange, brightorange, brightorange, brightorange, brightorange, brightorange, red, red, red, green, green, green, green],
+    ["#C1C1C1", brightorange, brightorange, brightorange, brightorange, brightorange, brightorange, brightorange, brightorange, brightorange, brightorange, brightorange, brightorange, brightorange, green, green, green, green],
+    ["#FFFFFF", green, brightorange, green, green, green, green, brightorange, brightorange, brightorange, brightorange, brightorange, brightorange, brightorange, green, green, green, green],
+    ["#C1C1C1", green, green, green, green, green, green, green, green, green, brightorange, brightorange, green, green, green, green, green, green]
+]
+let colorGridKirchheimerKreuzung = [
+    ["#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000"],
+    ["#C1C1C1", green, brightorange, yellow, yellow, brightorange, brightorange, darkorange, brightorange, brightorange, brightorange, yellow, brightorange, green, green, brightorange, green, green],
+    ["#FFFFFF", green, green, brightorange, brightorange, brightorange, brightorange, brightorange, brightorange, brightorange, darkorange, brightorange, brightorange, yellow, brightorange, green, brightorange, green],
+    ["#C1C1C1", yellow, yellow, brightorange, brightorange, brightorange, yellow, darkorange, yellow, brightorange, brightorange, brightorange, brightorange, yellow, green, darkorange, green, green],
+    ["#FFFFFF", yellow, yellow, brightorange, yellow, darkorange, brightorange, darkorange, brightorange, brightorange, brightorange, brightorange, brightorange, brightorange, yellow, brightorange, yellow, green],
+    ["#C1C1C1", brightorange, yellow, brightorange, brightorange, darkorange, brightorange, darkorange, brightorange, brightorange, brightorange, brightorange, yellow, yellow, brightorange, green, green, green],
+    ["#FFFFFF", green, yellow, brightorange, green, brightorange, brightorange, yellow, yellow, yellow, yellow, yellow, yellow, green, green, green, green, green],
+    ["#C1C1C1", green, green, yellow, green, yellow, brightorange, darkorange, brightorange, brightorange, brightorange, yellow, yellow, green, yellow, yellow, yellow, green]
+]
+let textGrid = [
+    ["Uhrzeit", 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22],
+    ["Montag"],
+    ["Dienstag"],
+    ["Mittwoch"],
+    ["Donnerstag"],
+    ["Freitag"],
+    ["Samstag"],
+    ["Sonntag"]
+]
+
+function createGrid(colorGrid, container) {
+    for (let i = 0; i < 18*8; i++) {
+        var gridItem = document.createElement("div");
+        gridItem.className = "grid-item";
+        gridItem.id = i.toString();
+        gridItem.setAttribute('onclick','select(this.id)');
+        var x = i % 18
+        var y = Math.floor(i/18)
+        if (y === 0) {
+            gridItem.style.color = "white"
+        }
+        if(colorGrid[y][x] !== undefined) {
+            gridItem.style.backgroundColor = colorGrid[y][x]
+        }
+        else {
+            gridItem.style.backgroundColor = "white";
+        }
+        if(textGrid[y][x] !== undefined) {
+            gridItem.innerHTML = textGrid[y][x];
+        }
+        else {
+            gridItem.innerHTML = "";
+        }
+        container.appendChild(gridItem);
+    }
+
+
+}
+function select(i) {
+    selectedGridItem = i;
+    const items = document.getElementsByClassName("grid-item");
+    for (let r = 0; r < items.length; r++) {
+        items.item(r).style.opacity = "1";
+    }
+    if (parseInt(i) === 0) {
+        return
+    }
+    if (parseInt(i) === selectedGridItem) {
+        return
+    }
+    const row = Math.floor(parseInt(i) / 18);
+    const column = parseInt(i) % 18;
+    for (let r = 0; r < items.length; r++) {
+        if((Math.floor(parseInt(r) / 18) % 8 !== row) && ((parseInt(r) % 18) !== column)) {
+            items.item(r).style.opacity = "0.5";
+        }
+    }
+
+}
+
+
+
+
+const Feldkirchner_Kreuzung_description = "Die Kreuzung in der Mitte der Gemeinde Feldkirchen ist von zentraler\n" +
+    "Bedeutung für Einwohner und Pendler. Vor allem unter der Woche ist sie von\n" +
+    "früh morgens ab 7 Uhr bis 20 Uhr abends konstant mittelstark ausgelastet. Am\n" +
+    "Abend zum Berufsverkehr hin kommt es oftmals zu stockendem Verkehr und\n" +
+    "gelegentlich zu kleinen Staus.\n" +
+    "Anlässlich der allgemein hohen Zuwachsrate der Bevölkerung im näheren\n" +
+    "Umkreis und dem einhergehenden stetigen Ausbau des ÖPNV mit neuen Busverbindungen\n" +
+    "ist in näherer Zukunft eine Zuspitzung des Verkehrs an dieser\n" +
+    "zentral gelegenen Kreuzung zu erwarten."
+
+const Ammerthalstrasse_description = "Die Kreuzung in der Mitte der Gemeinde Feldkirchen ist von zentraler\n" +
+    "Bedeutung für Einwohner und Pendler. Vor allem unter der Woche ist sie von\n" +
+    "früh morgens ab 7 Uhr bis 20 Uhr abends konstant mittelstark ausgelastet. Am\n" +
+    "Abend zum Berufsverkehr hin kommt es oftmals zu stockendem Verkehr und\n" +
+    "gelegentlich zu kleinen Staus.\n" +
+    "Anlässlich der allgemein hohen Zuwachsrate der Bevölkerung im näheren\n" +
+    "Umkreis und dem einhergehenden stetigen Ausbau des ÖPNV mit neuen Bus-\n" +
+    "verbindungen ist in näherer Zukunft eine Zuspitzung des Verkehrs an dieser\n" +
+    "zentral gelegenen Kreuzung zu erwarten."
+
+const Kirchheimer_Ei_description = "Das „Kirchheimer-Ei“ ist eine in der Gegend gut bekannte Bezeichnung für die viel befahrene ikonisch oval geformte Kreuzung der Staatsstraße-2082 mit dem Heimstettener-Moosweg. Sie steht schon seit vielen Jahren unter Kritik und wurde schon einige Male umgebaut. Diese Veränderungen haben bisher jedoch nicht viel an den teils chaotischen Zuständen verändert. Besonders am Morgen gegen 8 Uhr und am Abend von 16 bis 18 Uhr bildet sich ein enormer Rückstau an den beiden Kreuzungseinmündungen des Heimstettener-Mooswegs. Dieser führt teils bis zu über 30 Minuten Zeitverlust und ist für die Betroffenen nicht weiter hinnehmbar. Der Bau von neuen Wohnblocks in direkter Nähe zu der Kreuzung und die für 2024 geplante Landesgartenschau in der Gemeinde Kirchheim könnten zu einer weiteren Verschärfung der Situation an dieser Stelle führen."
+const Florianstrasse_description = "Die Florianstraße liegt zwischen dem Kirchheimer-Sportclub und dem Gewerbegebiet Kirchheims. In der eng gebauten Straße ist das Parken auf einer Fahrbahnseite erlaubt. Oftmals sind hier PKW, LKW, Busse und Fahrradfahrer gezwungen gegenseitig aufeinander zuwarten, um aneinander vorbeizukommen. Die Florianstraße kreuzt des Weiteren das Ende der viel befahrenen Staatsstraße-2082 und führt neben dem Kirchheimer Gewerbegebiet auch zu einem Wohnviertel. Dies resultiert in einem hohen Verkehrsaufkommen mit stockendem Verkehr in der Florianstraße.\n<br><br>" +
+    "Die Analyse zeigt unter anderem auch, dass generell am späten Abend und vor allem montags gegen 21 Uhr das Verkehrsaufkommen extrem stark ist und es teils zu Stau kommt. Dies kann natürlich nicht stimmen und beweist: Die von Google-Maps erhobenen Daten zum aktuellen Verkehr können auch Fehler enthalten. Vermutlich ist dieser explizite Fehler durch die vielen parkenden Autos des direktanliegenden Parkplatzes des KSC ausgelöst worden.\n"
+const Kirchheimer_Kreuzung_description = "Die zentral gelegene Hauptkreuzung Kirchheims ist ein Durchgangsknotenpunkt für viele Autofahrer aus den umliegenden Ortschaften. Die enge, aus Pflastersteinen gebaute Kreuzung ist tagsüber meist mittelstark befahren. Besonders zur Mittagszeit ist das Verkehrsaufkommen überdurchschnittlich hoch und es kommt zu stockendem Verkehr. Gerade die dort in engem Takt verkehrenden Busse der Linien 262, 263 und 263V kommen meist nur langsam um die Kurve und hindern den Verkehrsfluss. Fußgänger müssen sich mit den anderen Verkehrsteilnehmern aufgrund von nicht vorhandenen Zebrastreifen und Ampeln per Gestik oder Blickkontakt absprechen. Dies vereinfacht die Gesamtsituation an dieser Kreuzung nicht. Sie ist insgesamt nicht mehr für den heutigen Verkehr ausgelegt."
+
---
+++ scripts/verkehrszaehlung.js
@@ -0,0 +1,76 @@
+function submit() {
+    document.querySelectorAll('input[type=radio]').forEach(answer => {
+        answer.disabled = true;
+    });
+
+    const datasets = [
+        [document.querySelector('input[name="q1"]:checked'), "q1a3", document.getElementById("result1")],
+        [document.querySelector('input[name="q2"]:checked'), "q2a2", document.getElementById("result2")],
+        [document.querySelector('input[name="q3"]:checked'), "q3a3", document.getElementById("result3")],
+        [document.querySelector('input[name="q4"]:checked'), "q4a2", document.getElementById("result4")]
+    ]
+
+    let correct = 0;
+    for(let i = 0; i < datasets.length; i++) {
+        dataset = datasets[i];
+        document.getElementById(dataset[1]).nextElementSibling.style.color = "#006100";
+
+        if(dataset[0] === null) {
+            continue;
+        }
+
+        if(dataset[0].id === dataset[1]) {
+            dataset[2].innerHTML = "RICHTIG";
+            dataset[2].style.color = "#006100";
+            correct++;
+        } else {
+            dataset[2].innerHTML = "FALSCH";
+            dataset[2].style.color = "#9C0006";
+            dataset[0].nextElementSibling.style.color = "#9C0006";
+        }
+    }
+
+    const result = document.getElementById("result");
+    if(correct === 1) {
+        result.innerHTML = "Sie haben 1 Frage richtig beantwortet!";
+    } else {
+        result.innerHTML = "Sie haben " + correct + " Fragen richtig beantwortet!";
+    }
+    result.style.border = "3px solid black";
+    result.style.padding = "1.5vmin 0";
+
+    switch(correct) {
+        case 0:
+            result.style.backgroundColor = "#F0BBC2";
+            result.style.color = "#9C0006";
+            result.style.borderColor = "#9C0006";
+            break;
+        case 1:
+        case 2:
+        case 3:
+        case 4:
+            result.style.backgroundColor = "#C1E9C9";
+            result.style.color = "#006100";
+            result.style.borderColor = "#006100";
+            break;
+    }
+
+    document.getElementById("button").innerHTML = "Weiter";
+    document.getElementById("button").onclick = next;
+
+    window.scrollTo(0, 0);
+}
+
+function next() {
+    document.getElementById("quiz").style.display = "none";
+    document.getElementById("infos").style.display = "";
+    scrollTo(0, 0);
+}
+
+function scroll_to_question(question) {
+    scrollTo({
+        top: document.getElementById('quiz'.concat(question)).getBoundingClientRect().top + window.scrollY  - (screen.width < 750 ? 150 : 80),
+        left: 0,
+        behavior: 'smooth'
+    })
+}
